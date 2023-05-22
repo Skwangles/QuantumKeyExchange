0864bdfaea1fc50c08a8d1ea6fcf6b860b1398b0
skwangles <tree_house@Hotmail.co.nz>
2023-05-22 23:19:24 +1200

    Added XOR on string

--- src/XORciphering.java
+++ src/XORciphering.java
@@ -1,5 +1,20 @@
-public class XOR {
-    public int xor(int text, int key){
-        return text^key;
+public class XORciphering {
+    static public String xor(String text, String key){
+        char[] textChars = text.toCharArray();
+        char[] keyChars = key.toCharArray();
+
+        // XOR individual chars 1/0
+        for (int i = 0; i < textChars.length; i++){
+            char keyChar = keyChars[i % keyChars.length];
+            char textChar = textChars[i];
+
+            if ((textChar != '0' && textChar != '1') || (keyChar != '0' && keyChar != '1')){
+                throw new IllegalArgumentException("Input must be a binary string!");
+            }
+
+            textChars[i] = keyChar == textChar ? '0' : '1';
+        }
+
+        return String.valueOf(textChars);
     }
 }
